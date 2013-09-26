9569bfcce615786d11e9bc948a5d5cd5f15f1106
Ted Benson <edward.benson@gmail.com>
2013-09-26 16:45:25 -0400

    resources to support dev prod split

---
+++ src/fragments/development/constants.js
@@ -0,0 +1,4 @@
+_CTSUI.mockupBase = "http://localhost:8000/mockups/";
+_CTSUI.themeBase = "http://localhost:8000/mockups/";
+_CTSUI.cts-js-location = "http://localhost:9000/release/cts.js";
+
---
+++ src/fragments/production/constants.js
@@ -0,0 +1,3 @@
+_CTSUI.mockupBase = "http://treesheets.csail.mit.edu/mockups";
+_CTSUI.themeBase = "http://treesheets.csail.mit.edu/mockups/blog";
+_CTSUI.cts-js-location= "http://www.treesheets.org/cts.js";
