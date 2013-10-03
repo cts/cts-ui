dc84c6925dc3880ce5eaa5c26338c40908593411
Ted Benson <edward.benson@gmail.com>
2013-10-03 16:00:40 -0400

    special development env for wanger

---
+++ src/fragments/john/constants.js
@@ -0,0 +1,5 @@
+_CTSUI = {};
+
+_CTSUI.mockupBase = "http://wanger.mit.edu:8000/mockups/";
+_CTSUI.themeBase = "http://treesheets.csail.mit.edu/mockups/blog/";
+_CTSUI.ctsJsLocation = "http://wanger.mit.edu:9000/release/cts.js";
