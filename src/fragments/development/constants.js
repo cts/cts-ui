6621fb6106c550feb487cbcded8af74d963a4321
Ted Benson <edward.benson@gmail.com>
2013-10-04 01:53:52 -0400

    added hooks for server

--- src/fragments/development/constants.js
+++ src/fragments/development/constants.js
@@ -2,5 +2,6 @@ _CTSUI = {};
 
 _CTSUI.mockupBase = "http://localhost:8000/mockups/";
 _CTSUI.themeBase = "http://treesheets.csail.mit.edu/mockups/blog/";
+_CTSUI.serverBase = "http://localhost:8888/";
 _CTSUI.ctsJsLocation = "http://localhost:9000/release/cts.js";
 
