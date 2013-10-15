47f51796294c908f0c973c6a9a2b05aa56ed873f
Edward Benson <edward.benson@gmail.com>
2013-10-15 14:42:19 -0400

    alertify

--- src/modal.js
+++ src/modal.js
@@ -1,16 +1,21 @@
 /**
  * Modal Dialogue
  *
+ * This is currently a light-weight wrapper around a customized version of alertify.js.
+ * which has been modified to support asking the user to choose among options. This helps:
+ *   1. Provide a layer of encapsulation around third-party UI libraries, and
+ *   2. Provide the Q-based deferment method that CTS and CTS-UI use.
+ *
+ * Dependencies:
+ *  lib/alertify.js
+ *
  * Args:
- *  $ - jQuery (can be found at CTS.$ once CTS loads)
- *  q - The Q library (can be found at CTS.Q once CTS loads)
- *
- * TODO(ted): Args for what to show.
- * Serialize form?
- *
- * opts has the following components:
- *
- * 
+ *  $    - jQuery (can be found at CTS.$ once CTS loads)
+ *  q    - The Q library (can be found at CTS.Q once CTS loads)
+ *  opts - The options for this modal dialogue. This has the components:
+ *           title: The  title
+ *           body: The message to the user
+ *           yesno: (boolean) Whether to display an OK and Cancel option.
  *
  */
 _CTSUI.Modal = function($, q, opts) {
@@ -18,21 +23,8 @@ _CTSUI.Modal = function($, q, opts) {
   this._q = q;
   this._deferred = null;
 
-  // Default opts
-  this._opts = {
-    'title': "Question",
-    'body': "<p>Do you want to continue?</p>",
-    'buttons': [
-      {'label': 'OK'}
-    ]
-  };
-
-  // Override defaults with those passed in.
-  if (opt in opts) {
-    if (opts.hasOwnProperty(opt)) { // This suppresses prototypal members
-      this._opts[opt] = opts[opt];
-    }
-  }
+  // No defaults.
+  this._opts = opts || {};
 
 };
 
@@ -40,16 +32,57 @@ _CTSUI.Modal = function($, q, opts) {
  * Public methods
  *-----------------------------------------------------*/
 
-_CTSUI.Modal.prototype.isActive = function() {
-  return (this._deferred != null);
-};
-
 _CTSUI.Modal.prototype.show = function() {
-  if (this.isActive()) {
-    this.cancel();
-  }
   this._deferred = this._q.defer();
-  this._constructUI();
+  var deferred = this._deferred;
+
+  var hasTitle = ('title' in this._opts);
+  var hasBody = ('body' in this._opts);
+  var yesno = (('yesno' in this._opts) && (this._opts.yesno));
+  var hasChoices = ('choices' in this._opts);
+
+  var msg = null;
+
+  if (hasTitle && (! hasBody)) {
+    msg = this._opts.title;
+  } else if ((! hasTitle) && (hasBody)) {
+    msg = this._opts.body;
+  } else if (hasTitle && hasBody) {
+    msg = "<h2>" + this._opts.title + "</h2><div>" + this._opts.body + "</div>";
+  } else {
+    msg = "The programmer asked me to tell you something, but didn't tell me what to tell you!";
+  }
+
+
+  if (choices) {
+    var choices = {
+      'choices': this._opts.choices
+    };
+    if ('default' in this._opts) {
+      choices['default'] = this._opts['default'];
+    }
+    Alertify.dialog.choose(msg, function(e) {
+      if (e) {
+        deferred.resolve();
+      } else {
+        deferred.reject();
+      }
+    }, choices);
+  } else if (yesno) {
+    Alertify.dialog.confirm(msg, function(e) {
+      console.log(e);
+      if (e) {
+        deferred.resolve();
+      } else {
+        deferred.reject();
+      }
+    });
+  } else {
+    Alertify.dialog.alert(msg, function() {
+      deferred.resolve();
+    });
+  }
+
   return this._deferred.promise;
 };
 
@@ -62,15 +95,8 @@ _CTSUI.Modal.prototype.cancel = function() {
 };
 
 /*
- * User Interface
+ * Factory
  *-----------------------------------------------------*/
-
-_CTSUI.Modal.prototype._constructUI = function() {
-};
-
-_CTSUI.Modal.prototype._destroyUI = function() {
-};
-
 
 // TODO: Form-based stuff.
 _CTSUI.ModalFactory = {
