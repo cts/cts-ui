7b11a6cbbd77857c2da825a77b10e71c24ba4816
Ted Benson <edward.benson@gmail.com>
2013-11-20 22:07:52 -0500

    removing www subdomain from t.c.m.e

--- src/fragments/production/constants.js
+++ src/fragments/production/constants.js
@@ -1,8 +1,8 @@
 _CTSUI = {
   Domains: {
-    UIMockups: 'http://www.treesheets.csail.mit.edu/mockups/cts-ui',
-    CTS: 'http://www.treesheets.csail.mit.edu/hotlink/',
-    Server: 'http://www.treesheets.csail.mit.edu/',
-    Theme: 'http://www.treesheets.csail.mit.edu/mockups/blog/',
+    UIMockups: 'http://treesheets.csail.mit.edu/mockups/cts-ui',
+    CTS: 'http://treesheets.csail.mit.edu/hotlink/',
+    Server: 'http://treesheets.csail.mit.edu/',
+    Theme: 'http://treesheets.csail.mit.edu/mockups/blog/',
   }
 };
