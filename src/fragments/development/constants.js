abb17f1f30d56f0ae6f12018a3be914d05bf2a29
Oliver Song <osong@mit.edu>
2014-02-19 01:36:14 -0500

    Change to 3001

--- src/fragments/development/constants.js
+++ src/fragments/development/constants.js
@@ -3,6 +3,6 @@ _CTSUI = {};
 _CTSUI.mockupBase = "http://127.0.0.1:8000/mockups/";
 _CTSUI.themeBase = "http://treesheets.csail.mit.edu/mockups/blog/";
 _CTSUI.serverBase = "http://127.0.0.1:8888/";
-_CTSUI.ctsJsLocation = "http://127.0.0.1:9000/release/cts.js";
+_CTSUI.ctsJsLocation = "http://127.0.0.1:3001/release/cts.js";
 _CTSUI.switchboardPath = "tree/switchboard";
 _CTSUI.loginPath= "user/login";
